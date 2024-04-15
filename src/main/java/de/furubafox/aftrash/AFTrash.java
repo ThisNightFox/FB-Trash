3a1c0236439864800d56a7e0489c62acbe1e11ee
CraftCityHD <owner@citycrafting.de>
2024-04-15 19:25:51 +0200

    "trash" command added

--- src/main/java/de/furubafox/aftrash/AFTrash.java
+++ src/main/java/de/furubafox/aftrash/AFTrash.java
@@ -1,5 +1,6 @@
 package de.furubafox.aftrash;
 
+import de.furubafox.aftrash.command.TrashCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AFTrash extends JavaPlugin {
@@ -7,6 +8,7 @@ public final class AFTrash extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("Das Plugin wurde geladen");
+        getCommand("trash").setExecutor(new TrashCommand());
 
     }
 
