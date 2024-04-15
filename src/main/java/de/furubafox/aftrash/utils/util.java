338be672ff7faa966d393d1515adce83352de850
CraftCityHD <owner@citycrafting.de>
2024-04-15 22:22:43 +0200

    Added ItemBuilder & util

---
+++ src/main/java/de/furubafox/aftrash/utils/util.java
@@ -0,0 +1,14 @@
+package de.furubafox.aftrash.utils;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class util {
+    public static String Color(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    public static HashMap<Player, Player> tpas = new HashMap<Player, Player>();
+}
